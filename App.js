365def3bbc7548aa65e4914352fe04385c4cdd56
Jorge <jorgediazok@gmail.com>
2021-10-31 00:19:59 -0300

    created Cita component

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Cita from './components/Cita';
 
 export default function App() {
+  const [citas, setCitas] = useState([
+    {
+      id: '1',
+      paciente: 'Hook',
+      propietario: 'Juan',
+      sintomas: 'No Come',
+    },
+    {
+      id: '2',
+      paciente: 'Redux',
+      propietario: 'Manuel',
+      sintomas: 'No Duerme',
+    },
+    {
+      id: '3',
+      paciente: 'Native',
+      propietario: 'Masha',
+      sintomas: 'No Canta',
+    },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>This is the beginning</Text>
-      <StatusBar style='auto' />
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Administrador de Citas</Text>
+      <FlatList
+        data={citas}
+        renderItem={({ item }) => <Cita item={item} />}
+        keyExtractor={(cita) => cita.id}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
+    backgroundColor: '#AA076B',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titulo: {
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#FFF',
   },
 });
